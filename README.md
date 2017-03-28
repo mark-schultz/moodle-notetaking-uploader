e56107fb99bb62044b55d53de4d689eb0d936dd6
Mark Schultz <schultz.mark.d@gmail.com>
2017-03-28 13:23:43 -0700

    Remove old .py, edit README

--- README.md
+++ README.md
@@ -5,10 +5,12 @@ The script will:
 
 1. Find the most recent .pdf file (assuming it's named ClassName(#number), i.e. Real36).
 
+2. Preform a regex search over the corresponding .tex file to find the date (I store it as \lec{M/DD})
+
 2. Copy it to a "staging directory", open that directory in a graphical file explorer.
 
 3. Open the correct webpage for the class that you're taking notes for (to allow you to "drag and drop" from the graphical file explorer to the webpage).
 
-4. After a certain amount of time (~20 seconds), move the file from the staging directory elsewhere (could be easily changed to just delete it).
+4. Upon entering any input on the terminal, it will move the "staged" file to an archive subfolder of the staging directory.
 
-A lot of the things here (where the recent .pdf's are stored, where the staging directory is, the correct webpage is) is hard-coded.  Nevertheless, it allows uploading my notes to be a ~20 second mindless process instead of something much more involved.
+A lot of the things here (where the recent .pdf's are stored, where the staging directory is, the correct webpage is) is hard-coded.  Nevertheless, it allows uploading my notes to be a ~10 second process.
--- notecopy.py
+++ notecopy.py
@@ -1,88 +1,127 @@
-#!/usr/bin/env python
-import os, shutil, time, webbrowser, glob
+import time
+import os
+import glob
+import shutil
+import webbrowser
+import atexit
+import subprocess
+import sys
+import re
 
-# Made by Mark Schultz for automating
-# uploading notes after class.
+FILE_PATHS = ["/home/mark/classes/", "-Spring-2017/notes/"]
+DEST_PATH = "/home/mark/Desktop/Notetaking/"
+EXTRACT_DIGITS_RE = re.compile(r"\d+")
+DATE_FROM_TEX_RE = re.compile("\\lec\{(\d+)/(\d+)\}")
+
 
 def find_ext(dr, ext):
+    """
+    Return a list of all files with a given file extension in a folder.
+    """
     return glob.glob(os.path.join(dr, "*." + ext))
-# Returns all files of a given extension in the folder.
 
-def prepareNotes(course):
-    # Copies the .pdf of notes from my LaTeX folder to the
-    # NoteTaking folder, renames it to the proper format,
-    # And opens Moodle to the right page to make it easy to
-    # upload.
 
-    ## Find highest number in a .pdf file, e.g. 23 in /year3/real/Real23.pdf
-    beginning = "/home/mark/latex/year3/"
-    src = os.path.join(beginning,course[1].lower())
-    pdfs = find_ext(src,"pdf")  ## Finds all .pdfs in /year3/coursename/ folder
-    lenCut = len(beginning)
-    ## Removes /home/mark/latex/year3/ from filepath of each .pdf
-    ##
-    for val in pdfs:
-        val = val[lenCut+1:]
-    maximum = -1
-    maximumindex = -1
-    outputPdfs = pdfs[:]
-    ## Iterates through all the RealXX.pdf names, finds the highest XX
-    i = 0
-    while i < len(pdfs):
-        pdfs[i] = ''.join([c for c in pdfs[i] if c in '1234567890'])
-        if int(pdfs[i])>maximum:
-            maximum = int(pdfs[i])
-            maximumindex = i
-        i += 1
-    fileToCopy = outputPdfs[maximumindex]
-    ## Gets the absolute filepath of the file
-    src = os.path.join(src,fileToCopy)
-    dst = "/home/mark/Desktop/Notetaking/"
-    ## Formats the date correctly for renaming
-    date = time.strftime("%x").lstrip("0").replace("/","-")
-    ## Renames the file in the format desired
-    filename = course[0]+" Notes "+date+".pdf"
-    shutil.copy(fileToCopy,dst+os.sep+filename)
-    ## Open moodle page for uploading the notes
-    webbrowser.open_new("https://moodle.reed.edu/"+course[2])
-    ## Open the GUI file browser where the notes get copied to temporarily
-    webbrowser.open('/home/mark/Desktop/Notetaking')
-    ## Timer Section
-    ## For copying the notes to the archive folder
-    timeToExit = 20
-    time.sleep(2)
-    timeToExit -= 2
-    print("Time remaining:")
-    while timeToExit>0:
-        if timeToExit % 2 == 0:
-            print(timeToExit)
-        time.sleep(1)
-        timeToExit -= 1
-    shutil.move(dst+os.sep+filename,dst+os.sep+course[1]+os.sep+filename)
-    print("File moved")
-    print("Safe to Close")
-def chooseClass(classes):
-    # Asks which course to apply prepareNotes to
-    # if there are multiple courses.
-    if len(classes) == 1:
-        choice = 0
+def move_renamed_file(old_file_path, new_file_path):
+    """
+    Should move the staged .pdf to an "archive" folder on exit.
+    """
+    shutil.move(old_file_path, new_file_path)
+
+
+def get_number_in_string(string, default=-1):
+    """
+    Given a string that contains a number, returns that number.
+
+    String expected to be of the form <word>XY.pdf, where <word> contains no numbers.
+
+    """
+    find = EXTRACT_DIGITS_RE.search(string)
+    if find is None:
+        return default
     else:
-        i = 0
-        print("Which class are the Notes For?")
-        while i < len(classes):
-            print(str(i+1)+" - "+classes[i][1])
-            i += 1
-        choice = int(input("Choose: "))
-        choice = choice - 1
-    return prepareNotes(classes[choice])
+        return int(find.group())
 
-# Put lists for classes here
-Real = ["MTH 321","Real","mod/data/view.php?id=40252"]
 
-# Assemble the classes together and sort by title (e.g. "Real")
-classes = []
-classes.append(Real)
-classes.sort(key = lambda x: x[1])
+class Course:
 
-# Actually run things here
-chooseClass(classes)
+    def __init__(self, class_name, class_schedule, subject, term, course_number, moodle_id):
+        self.directory = "".join(
+            [FILE_PATHS[0], "/", term, "/", class_name, FILE_PATHS[1]])
+        self.class_schedule = class_schedule
+        self.subject = subject
+        self.course_number = str(course_number)
+        self.moodle_id = str(moodle_id)
+
+    def make_new_name(self, date=None):
+        """
+        Makes a new name of form SUBJ NUM MM-DD-YYYY.pdf, where SUBJ is the department the class is in (i.e. MATH), NUM is the three-digit class number, and MM-DD-YYYY is the most recent date.
+        """
+        if date is None:
+            date = time.strftime("%x")
+        formatted_date = date.lstrip("0").replace("/", "-")
+        return "".join([self.subject, " ", self.course_number, " ", formatted_date, ".pdf"])
+
+    def find_highest_pdf(self):
+        """
+        Finds the .pdf with file name containing the highest number in the directory.
+        """
+        pdfs = find_ext(self.directory, "pdf")
+        last_backslash = pdfs[0].rfind("/")
+        for num in range(len(pdfs)):
+            pdfs[num] = pdfs[num][last_backslash + 1:]
+        pdfs.sort(key=get_number_in_string)
+        self.target_pdf = pdfs[-1]
+        return "".join([self.directory, self.target_pdf])
+
+    def find_tex_from_pdf(self, pdf_directory):
+        """
+        Given a file-path to a .pdf file, returns the .tex file corresponding to it.
+        """
+        return pdf_directory.split('.')[0] + '.tex'
+
+    def get_date_from_tex(self, tex):
+        """
+        Given a file-path to a .tex file, opens it and looks for \lec{A/BC}, which is the date it was written.
+
+        Returns MM/DD/YYYY
+        """
+        with open(tex, 'r') as f:
+            string = f.read()
+        date = DATE_FROM_TEX_RE.search(string).groups()
+        return "/".join([date[0].zfill(2), date[1], time.strftime("%Y")])
+
+    def move_file(self, file_name, date):
+        """
+        Takes a file, renames it, and moves it to the "staging" folder.
+        """
+        new_file_name = self.make_new_name(date)
+        dest = os.path.join(DEST_PATH, new_file_name)
+        shutil.copy(file_name, dest)
+
+    def open_browser(self):
+        """
+        Opens the webpage and "staging" folder to allow drag and drop of the .pdf to the website.
+        """
+        webbrowser.open_new(
+            "".join(["https://moodle.reed.edu/mod/data/view.php?id=", self.moodle_id]))
+        subprocess.Popen(["xdg-open", DEST_PATH])
+
+    def upload_notes(self):
+        """
+        Runs all the prior functions in the right order.
+        """
+        pdf = self.find_highest_pdf()
+        tex = self.find_tex_from_pdf(pdf)
+        date = self.get_date_from_tex(tex)
+        new_file_name = self.make_new_name(date)
+        self.move_file(pdf, date)
+        self.open_browser()
+        atexit.register(move_renamed_file, "".join([DEST_PATH, new_file_name]), "".join(
+            [DEST_PATH, self.course_number, " archive/", new_file_name]))
+        input("Press any key to exit")
+        sys.exit()
+
+
+if __name__ == "__main__":
+    crypto = Course("cryptography", "MWF", "MATH", "Sp17", 388, 51406)
+    crypto.upload_notes()

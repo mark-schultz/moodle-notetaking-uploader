8ad8aea19cb55cace6e54d3ad154e065680d29f4
Mark Schultz <schultz.mark.d@gmail.com>
2017-03-29 00:28:51 -0700

    More progress, passing json now that's being rejected

--- custom_requests.py
+++ custom_requests.py
@@ -9,10 +9,12 @@ SESS_KEY_RE = re.compile(r"\"sesskey\":\"(\w*)\"")
 ITEM_ID_RE = re.compile(r"\"itemid\":(\d*),")
 AUTHOR_RE = re.compile(r"\"author\":\"([^\"]+)\"")
 CTX_RE = re.compile(r";ctx_id=(\d+)&amp;")
+CLIENT_ID_RE = re.compile(r"\"client_id\":\"(\w*)\"")
 ##
 
 LOGIN_URL = 'https://weblogin.reed.edu/'
 SUBMIT_URL = 'https://moodle.reed.edu/mod/data/edit.php?d=485'
+DRAFT_FILES_URL = 'https://moodle.reed.edu/repository/draftfiles_ajax.php?action=list'
 submit_url = 'https://moodle.reed.edu/repository/repository_ajax.php?action=upload'
 TITLE = r"MATH 389 3-20-17.pdf"
 FILENAME = r"/home/mark/classes/moodle-notetaking-uploader/MATH 389 3-20-17.pdf"
@@ -22,6 +24,7 @@ FILENAME = r"/home/mark/classes/moodle-notetaking-uploader/MATH 389 3-20-17.pdf"
 with open(FILENAME, 'rb') as f:
     s = requests.Session()
     s.get(SUBMIT_URL)
+    # Removing above breaks this, not sure why
     r = s.post(LOGIN_URL, data=CREDENTIALS)
     l = s.get(SUBMIT_URL)
     text = l.content.decode()
@@ -29,8 +32,12 @@ with open(FILENAME, 'rb') as f:
     item_id = ITEM_ID_RE.search(text).group(1)
     author = AUTHOR_RE.search(text).group(1)
     ctx_id = CTX_RE.search(text).group(1)
+    client_id = CLIENT_ID_RE.search(text).group(1)
+    draft_files_payload = {"sesskey": sess_key, "client_id": client_id,
+                           "filepath": "/", "itemid": item_id}
     file_payload = {"file": ("repo_upload_file", f, "application/pdf")}
-    data_payload = {"repo_id": "4", "author": author,
-                    "title": TITLE, "ctx_id": ctx_id, 'savepath': '/',
-                    "sesskey": sess_key, "itemid": item_id}
-    q = s.post(submit_url, files=file_payload, data=data_payload)
+    data_payload = {"sesskey": sess_key, "repo_id": "4", "itemid": item_id,
+                    "author": author, "savepath": "/", "title": TITLE,
+                    "ctx_id": ctx_id}
+    a = s.post(DRAFT_FILES_URL, json=draft_files_payload)
+    b = s.post(submit_url, files=file_payload, data=data_payload)

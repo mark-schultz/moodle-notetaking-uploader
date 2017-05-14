82f165e5a02239af323d97d35748443ce9088374
Mark Schultz <schultz.mark.d@gmail.com>
2017-05-14 13:50:45 -0700

    Issue with Moodle - halt progress

--- custom_requests.py
+++ custom_requests.py
@@ -1,4 +1,5 @@
 import requests
+from requests_toolbelt.multipart.encoder import MultipartEncoder
 import http.client
 from credentials import CREDENTIALS
 import re
@@ -15,29 +16,35 @@ CLIENT_ID_RE = re.compile(r"\"client_id\":\"(\w*)\"")
 LOGIN_URL = 'https://weblogin.reed.edu/'
 SUBMIT_URL = 'https://moodle.reed.edu/mod/data/edit.php?d=485'
 DRAFT_FILES_URL = 'https://moodle.reed.edu/repository/draftfiles_ajax.php?action=list'
-submit_url = 'https://moodle.reed.edu/repository/repository_ajax.php?action=upload'
+UPLOAD_URL = 'https://moodle.reed.edu/repository/repository_ajax.php?action=upload'
 TITLE = r"MATH 389 3-20-17.pdf"
 FILENAME = r"/home/mark/classes/moodle-notetaking-uploader/MATH 389 3-20-17.pdf"
 
-
+def regex_for_string(string):
+    reg = re.compile(string)
+    def tmp(string_to_search):
+        return reg.search(str(string_to_search))
+    return tmp
 # repo_id 4 seems to correspond with "upload a file", look at "sortorder":4
 with open(FILENAME, 'rb') as f:
     s = requests.Session()
-    s.get(SUBMIT_URL)
+    moodle_page_load = s.get(SUBMIT_URL)
     # Removing above breaks this, not sure why
-    r = s.post(LOGIN_URL, data=CREDENTIALS)
-    l = s.get(SUBMIT_URL)
-    text = l.content.decode()
+    login_query = s.post(LOGIN_URL, data=CREDENTIALS)
+    second_moodle_page_load = s.get(SUBMIT_URL)
+    text = second_moodle_page_load.content.decode()
     sess_key = SESS_KEY_RE.search(text).group(1)
+    sess_search = regex_for_string(sess_key)
     item_id = ITEM_ID_RE.search(text).group(1)
     author = AUTHOR_RE.search(text).group(1)
     ctx_id = CTX_RE.search(text).group(1)
     client_id = CLIENT_ID_RE.search(text).group(1)
-    draft_files_payload = {"sesskey": sess_key, "client_id": client_id,
-                           "filepath": "/", "itemid": item_id}
+    draft_header = {'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8'}
+    draft_files_payload = MultipartEncoder(fields = (("sesskey", sess_key), ("client_id", client_id),
+                           ("filepath", "/"), ("itemid", item_id)))
     file_payload = {"file": ("repo_upload_file", f, "application/pdf")}
     data_payload = {"sesskey": sess_key, "repo_id": "4", "itemid": item_id,
                     "author": author, "savepath": "/", "title": TITLE,
                     "ctx_id": ctx_id}
-    a = s.post(DRAFT_FILES_URL, json=draft_files_payload)
-    b = s.post(submit_url, files=file_payload, data=data_payload)
+    draft_query = s.post(DRAFT_FILES_URL, data=draft_files_payload,headers = draft_header)
+    upload_query = s.post(UPLOAD_URL, files=file_payload, data=data_payload)
